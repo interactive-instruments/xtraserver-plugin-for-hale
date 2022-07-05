d890820af0b61e3c99dcbe9b91692f2c59deaf7f
Johannes Echterhoff <echterhoff@interactive-instruments.de>
2022-07-05 17:15:04 +0200

    CustomFunctionAdvToGeographicalNameSimple
    closes #11
    
    Signed-off-by: Johannes Echterhoff <echterhoff@interactive-instruments.de>

--- plugins/xtraserver-hale-io/src/main/java/de/ii/xtraserver/webapi/hale/io/writer/handler/CustomFunctionAdvToGeographicalNameSimple.java
+++ plugins/xtraserver-hale-io/src/main/java/de/ii/xtraserver/webapi/hale/io/writer/handler/CustomFunctionAdvToGeographicalNameSimple.java
@@ -17,8 +17,15 @@ package de.ii.xtraserver.webapi.hale.io.writer.handler;
 
 import de.ii.xtraplatform.features.domain.FeatureSchema;
 import de.ii.xtraplatform.features.domain.ImmutableFeatureSchema;
+import de.ii.xtraplatform.features.domain.SchemaBase;
+import de.ii.xtraplatform.features.domain.transform.ImmutablePropertyTransformation;
+import de.ii.xtraserver.hale.io.writer.XtraServerMappingUtils;
+import de.ii.xtraserver.webapi.hale.io.writer.XtraServerWebApiTypeUtil;
 import eu.esdihumboldt.hale.common.align.model.Cell;
 import eu.esdihumboldt.hale.common.align.model.Property;
+import eu.esdihumboldt.hale.common.core.io.Value;
+import eu.esdihumboldt.hale.common.schema.model.PropertyDefinition;
+import eu.esdihumboldt.hale.common.schema.model.TypeDefinition;
 import java.util.Optional;
 
 /**
@@ -29,8 +36,6 @@ import java.util.Optional;
 class CustomFunctionAdvToGeographicalNameSimple extends FormattedStringHandler {
 
 	public final static String FUNCTION_ID = "custom:alignment:adv.inspire.GeographicalName.simple";
-	private final static String XSI_NS = "http://www.w3.org/2001/XMLSchema-instance";
-	private final static String XLINK_NS = "http://www.w3.org/1999/xlink";
 
 	CustomFunctionAdvToGeographicalNameSimple(final MappingContext mappingContext) {
 		super(mappingContext);
@@ -43,7 +48,19 @@ class CustomFunctionAdvToGeographicalNameSimple extends FormattedStringHandler {
 	@Override
 	public Optional<ImmutableFeatureSchema.Builder> doHandle(final Cell propertyCell, final Property targetProperty) {
 
-		return Optional.empty();
+		ImmutableFeatureSchema.Builder typeBuilder = mappingContext.getFeatureBuilder();
+		ImmutableFeatureSchema.Builder propertyBuilder = new ImmutableFeatureSchema.Builder();
+		String pName = "name_deu";
+		propertyBuilder.name(pName);
+		typeBuilder.getPropertyMap().put(pName,propertyBuilder);
+
+		Property sourceProperty = XtraServerMappingUtils.getSourceProperty(propertyCell);
+		String sourcePath = propertyName(sourceProperty);
+		propertyBuilder.sourcePath(sourcePath);
+
+		propertyBuilder.type(SchemaBase.Type.STRING);
+
+		return Optional.of(propertyBuilder);
 	}
 
 }
